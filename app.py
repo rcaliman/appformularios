09b016ca0f86161d9185a40178cf8dab6765971a
Rodrigo Caliman <rcaliman@gmail.com>
2021-10-25 22:43:04 -0300

    refatorando e melhorando a formatacao

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request
-from numero_por_extenso import monetario
-from util import data_extenso, cpf_cnpj, formata_processo, formata_data
+from util import data_extenso, cpf_cnpj, formata_processo, formata_data, formata_moeda, valor_por_extenso
 
 app = Flask(__name__)
 
@@ -17,11 +16,13 @@ def declaracao_de_isencao_irpf():
 
 @app.route('/imprimir_declaracao_de_isencao_irpf', methods=['POST'])
 def imprimir_declaracao_de_isencao_irpf():
-    valor_extenso = monetario(request.form.get('valornumero').replace('.', ''))
+    salario = formata_moeda(request.form.get('valornumero'))
+    valor_extenso = valor_por_extenso(salario)
     cpf_cnpj_formatado = cpf_cnpj(request.form.get('cpf-cnpj'))
     return render_template('imprimir_declaracao_de_isencao_irpf.html',
                            dados=request.form.to_dict(),
                            valor_extenso=valor_extenso,
+                           salario=salario,
                            data_extenso=data_extenso(),
                            cpf_cnpj_formatado=cpf_cnpj_formatado,
                            formata_processo=formata_processo(request.form.get('processo'))
@@ -35,13 +36,14 @@ def declaracao_de_renda():
 
 @app.route('/imprimir_declaracao_de_renda', methods=['POST'])
 def imprimir_declaracao_de_renda():
-    valor_extenso = monetario(request.form.get('salario').replace('.', ''))
     cpf = cpf_cnpj(request.form.get('cpf'))
+    salario = formata_moeda(request.form.get('salario'))
     return render_template('imprimir_declaracao_de_renda.html',
                            dados=request.form.to_dict(),
-                           valor_extenso=valor_extenso,
+                           valor_extenso=valor_por_extenso(salario),
                            cpf=cpf,
                            data_extenso=data_extenso(),
+                           salario=salario,
                            ci_emissao=formata_data(request.form.get('ci_emissao')),
                            data1_salario=formata_data(request.form.get('data1_salario')),
                            data2_salario=formata_data(request.form.get('data2_salario')),
--- util.py
+++ util.py
@@ -1,26 +1,77 @@
 from datetime import datetime
+from num2words import num2words
 
 
-def data_extenso():
+def valor_por_extenso(numero: str) -> str:
+    """
+    usa a biblioteca num2words para transformar o valor em extenso
+    :param numero: str
+    :return: str
+    """
+    valor, centavos = numero.split(',')
+    valor = valor.replace('.', '')
+    reais = num2words(valor, lang='pt_BR')
+    if centavos == '00':
+        texto_centavos = ''
+    elif centavos == '01':
+        texto_centavos = ' e um centavo'
+    else:
+        texto_centavos = ' e ' + num2words(centavos, lang='pt_BR') + ' centavos'
+    return reais + ' reais' + texto_centavos
+
+
+def data_extenso() -> str:
+    """
+    transforma a data de numeral para extenso
+    :return: str
+    """
     mes = 'janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho',\
           'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'
 
     return f'Colatina-ES, {datetime.today().day} de {mes[datetime.today().month]} de {datetime.today().year}'
 
 
-def formata_data(data):
+def formata_data(data: str) -> str:
+    """
+    formata a data dividindo os campos por /
+    :param data: str
+    :return: str
+    """
+    print(type(data))
     if len(str(data)) > 6:
         return f'{data[-8:-6]}/{data[-6:-4]}/{data[-4:]}'
     else:
         return f'{data[-6:-4]}/{data[-4:]}'
 
 
-def cpf_cnpj(_cpf_cnpj):
+def cpf_cnpj(_cpf_cnpj: str) -> str:
+    """
+    formata o cpf ou cnpj dividindo o numero pelos campos
+    :param _cpf_cnpj: str
+    :return: str
+    """
     if len(str(_cpf_cnpj)) <= 11:
         return f'{_cpf_cnpj[:-8]}.{_cpf_cnpj[-8:-5]}.{_cpf_cnpj[-5:-2]}-{_cpf_cnpj[-2:]}'
     else:
         return f'{_cpf_cnpj[:-12]}.{_cpf_cnpj[-12:-9]}.{_cpf_cnpj[-9:-6]}/{_cpf_cnpj[-6:-2]}-{_cpf_cnpj[-2:]}'
 
 
-def formata_processo(_processo):
+def formata_processo(_processo: str) -> str:
+    """
+    formata o numero do processo divindo o numero pelos devidos campos
+    :param _processo: str
+    :return: str
+    """
     return f'{_processo[:-11]}.{_processo[-11:-9]}.{_processo[-9:-7]}.{_processo[-7:-1]}-{_processo[-1:]}'
+
+
+def formata_moeda(_valor: str) -> str:
+    """
+    formata moeda para o padrão brasileiro
+    :param _valor: str
+    :return: str
+    """
+    centavos = _valor[-2:]
+    inteiros = int(_valor[:-2])
+    return ('{:,}'.format(inteiros)).replace(',', '.') + ',' + centavos
+
